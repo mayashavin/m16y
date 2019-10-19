64acc10fc8f86ff4927a8aaa9e39488a68ad9855
Maya Shavin <maya@cloudinary.com>
2019-10-19 22:56:31 +0300

    add members

---
+++ src/components/Member.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let member = {};
+
+  const avatarUrl = `https://github.com/${member.github}.png`
+</script>
+<article class="flex items-center my-3">
+  <div class="w-16 h-16">
+    <img src={avatarUrl} alt={member.name} class="rounded-full"/>
+  </div>
+  <div class="ml-2">
+    <h3>{member.name}</h3>
+  </div>
+</article>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,22 @@
+<script>
+  import Member from "../components/Member.svelte";
+  const members = [{
+    name: "Maya Shavin",
+    github: "mayashavin"
+  }, {
+    name: "Tamas Piros",
+    github: "tpiros"
+  }];
+</script>
 <svelte:head>
 	<title>About</title>
 </svelte:head>
-
-<h1>About this site</h1>
-
-<p>This is the 'about' page. There's not much here.</p>
+<div class="flex flex-col items-center mt-10">
+  <h1 class="text-2xl text-center pb-2 border-b-2">About the project</h1>
+  <p class="pt-2">This project is currently maintained by:</p>
+  <div class="my-3 mx-2">
+   {#each members as member}
+    <Member member={member}/>
+    {/each}
+  </div>
+</div> 
